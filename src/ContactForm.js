96c182722a8b71eb4fdfe284594a737ff5c7e29d
mohamed-a95 <mohamedabdihakima95@gmail.com>
2024-06-18 01:50:34 +0200

    flera fält har placerats i formuläret samt bättre styling

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 function ContactForm() {
+  const [selectedQuery, setSelectedQuery] = useState("");
+
   const {
     register,
     handleSubmit,
@@ -11,6 +14,10 @@ function ContactForm() {
     console.log(data);
   };
 
+  const handleClick = (type) => {
+    setSelectedQuery(type);
+  };
+
   return (
     <div className="contain">
       <h2>Contact Form</h2>
@@ -18,14 +25,14 @@ function ContactForm() {
         <div className="inp1">
           <label>Name</label>
           <br />
-          <input {...register("name", { required: true })} />
+          <input type="text" {...register("firstname", { required: true })} />
           <br />
           {errors.name && <span>This field is required</span>}
         </div>
         <div className="inp2">
           <label>Last name</label>
           <br />
-          <input {...register("name", { required: true })} />
+          <input type="text" {...register("lastname", { required: true })} />
           <br />
           {errors.name && <span>This field is required</span>}
         </div>
@@ -37,6 +44,36 @@ function ContactForm() {
           />
           <br />
           {errors.email && <span>Invalid email address</span>}
+        </div>
+        <label className="querytype">Query type</label>
+        <div className="inp4">
+          <div className={selectedQuery === "general" ? "inp4-12" : "inp4-1"}>
+            <input
+              type="radio"
+              checked={selectedQuery === "general"}
+              onClick={() => handleClick("general")}
+              {...register("selectedQuery", { requierd: true })}
+            />
+            <label>General Enquiry</label>
+          </div>
+          <div className={selectedQuery === "support" ? "inp4-12" : "inp4-1"}>
+            <input
+              type="radio"
+              checked={selectedQuery === "support"}
+              {...register("selectedQuery", { requierd: true })}
+              onClick={() => handleClick("support")}
+            />
+            <label>Support request</label>
+          </div>
+        </div>
+        <label className="messageLabel">Message</label>
+        <div className="inp5">
+          <input
+            type="text"
+            {...register("message", { requierd: true })}
+            min={10}
+            max={100}
+          />
         </div>
         <input type="submit" />
       </form>
